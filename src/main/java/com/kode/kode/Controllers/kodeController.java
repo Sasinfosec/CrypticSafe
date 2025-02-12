1a744ef318d07278c59eed457df1c7e4551d9930
sasinfosec@gmail.com <sasinfosec@gmail.com>
2025-02-12 18:03:51 +0000

    Added CORS For Supporting Frontend

--- src/main/java/com/kode/kode/Controllers/kodeController.java
+++ src/main/java/com/kode/kode/Controllers/kodeController.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") 
 @RequestMapping("/api/v1")
 public class kodeController {
     @GetMapping("/")
